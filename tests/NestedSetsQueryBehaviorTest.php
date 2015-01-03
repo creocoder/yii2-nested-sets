bcc816c0178ef1ba0166c576f67bb31b4b61aeca
Alexander Kochetov <creocoder@gmail.com>
2015-01-03 16:18:21 +0300

    Added unit tests base for NestedSetsQueryBehavior

---
+++ tests/NestedSetsQueryBehaviorTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-nested-sets-behavior
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace tests;
+
+/**
+ * NestedSetsQueryBehaviorTest
+ */
+class NestedSetsQueryBehaviorTest extends DatabaseTestCase
+{
+    /**
+     * @covers \creocoder\nestedsets\NestedSetsQueryBehavior::roots
+     */
+    public function testRoots()
+    {
+        $this->markTestSkipped();
+    }
+
+    /**
+     * @covers \creocoder\nestedsets\NestedSetsQueryBehavior::leaf
+     */
+    public function testLeaf()
+    {
+        $this->markTestSkipped();
+    }
+}
