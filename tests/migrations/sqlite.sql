194454c216c318ccc18d30b23fc33cbd56bdbe21
Alexander Kochetov <creocoder@gmail.com>
2015-01-06 00:02:51 +0300

    Reformat migrations sql file

--- tests/migrations/sqlite.sql
+++ tests/migrations/sqlite.sql
@@ -5,20 +5,20 @@
 DROP TABLE IF EXISTS "tree";
 
 CREATE TABLE "tree" (
-  "id" INTEGER NOT NULL PRIMARY KEY,
-  "lft" INTEGER NOT NULL,
-  "rgt" INTEGER NOT NULL,
+  "id"    INTEGER NOT NULL PRIMARY KEY,
+  "lft"   INTEGER NOT NULL,
+  "rgt"   INTEGER NOT NULL,
   "depth" INTEGER NOT NULL,
-  "name" TEXT NOT NULL
+  "name"  TEXT    NOT NULL
 );
 
 DROP TABLE IF EXISTS "multiple_roots_tree";
 
 CREATE TABLE "multiple_roots_tree" (
-  "id" INTEGER NOT NULL PRIMARY KEY,
-  "tree" INTEGER,
-  "lft" INTEGER NOT NULL,
-  "rgt" INTEGER NOT NULL,
+  "id"    INTEGER NOT NULL PRIMARY KEY,
+  "tree"  INTEGER,
+  "lft"   INTEGER NOT NULL,
+  "rgt"   INTEGER NOT NULL,
   "depth" INTEGER NOT NULL,
-  "name" TEXT NOT NULL
+  "name"  TEXT    NOT NULL
 );
