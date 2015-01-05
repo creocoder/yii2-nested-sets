21d250ce05142c7546d9e2041e6e8f6906463c5f
Alexander Kochetov <creocoder@gmail.com>
2015-01-05 11:03:13 +0300

    Changed files header link to repo

--- tests/DatabaseTestCase.php
+++ tests/DatabaseTestCase.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @link https://github.com/creocoder/yii2-nested-sets-behavior
+ * @link https://github.com/creocoder/yii2-nested-sets
  * @copyright Copyright (c) 2015 Alexander Kochetov
  * @license http://opensource.org/licenses/BSD-3-Clause
  */
